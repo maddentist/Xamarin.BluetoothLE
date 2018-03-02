75b528c95f300f698de6a2de013e793f7013b4ae
Mathieu A Derbanne <mathieu@airprox.aero>
2018-03-02 11:29:01 +0100

    Converted to .NET standard

---
+++ BluetoothLE.Example.Net/BluetoothLE.Example.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Xamarin.Forms;
+using BluetoothLE.Example.Pages;
+using BluetoothLE.Core;
+
+namespace BluetoothLE.Example
+{
+	public class App : Application
+	{
+		private static readonly IAdapter _bluetoothAdapter;
+		public static IAdapter BluetoothAdapter { get { return _bluetoothAdapter; } }
+
+		static App() {
+			_bluetoothAdapter = DependencyService.Get<IAdapter>();
+
+			_bluetoothAdapter.ScanTimeout = TimeSpan.FromSeconds(10);
+			_bluetoothAdapter.ConnectionTimeout = TimeSpan.FromSeconds(10);
+		}
+
+		public App()
+		{
+			// The root page of your application
+			MainPage = new NavigationPage(new DeviceListPage())
+			{
+				Title = "Bluetooth LE Explorer"
+			};
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
